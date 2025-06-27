6fe97db6dd5f4b5e2c6688cf6acc78202adfa008
Lalitha-Kalle <kallelalitha04@gmail.com>
2025-06-28 00:00:19 +0530

    added routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,27 @@
+// const User = require('../models/User');
+// const { fetchLeetCodeData } = require('../services/leetcodeService');
+// const { calculateScore } = require('../utils/scorer');
+
+// const registerUser = async (req, res) => {
+//   const { email, leetcodeUsername } = req.body;
+//   try {
+//     const exists = await User.findOne({ leetcodeUsername });
+//     if (exists) return res.status(409).json({ message: "User already exists" });
+
+//     const stats = await fetchLeetCodeData(leetcodeUsername);
+//     const score = calculateScore(stats);
+
+//     const newUser = await User.create({ email, leetcodeUsername, score });
+//     res.status(201).json(newUser);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// };
+
+
+
+// module.exports = { registerUser }
+
 const User = require('../models/User');
 const { fetchLeetCodeData } = require('../services/leetcodeService');
 const { calculateScore } = require('../utils/scorer');
@@ -18,6 +42,9 @@ const registerUser = async (req, res) => {
   }
 };
 
+const getLeaderboard = async (req, res) => {
+  const users = await User.find().sort({ score: -1 }).limit(50);
+  res.json(users);
+};
 
-
-module.exports = { registerUser }
+module.exports = { registerUser, getLeaderboard };
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ const dotenv = require("dotenv")
 dotenv.config()
 const express = require("express")
 require("./db/db")
-
+const userRoutes = require("./routes/user.routes")
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use("/api/users", userRoutes)
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to Home page")
